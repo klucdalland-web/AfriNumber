c8335842de4e5539f9686c2cf5f1b02ca4d8b2d3
klucdalland-web <klucdalland@gmail.com>
2026-09-24 12:31:57 +0100

    feat: init firebase & controller getx & routing getx

--- afri_number/lib/main.dart
+++ afri_number/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -5,9 +6,13 @@ import 'package:get_storage/get_storage.dart';
 import 'app/bindings/initial_binding.dart';
 import 'app/routes/app_pages.dart';
 import 'app/theme/app_theme.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await GetStorage.init();
   runApp(const AfriNumberApp());
 }
